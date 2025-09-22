ec54c6e6abcc1246c7a56afbe504c22312d574f2
Mauricio Robert <188394952+Nistezy@users.noreply.github.com>
2025-09-21 21:31:46 -0300

    Update players.js

--- players.js
+++ players.js
@@ -1,24 +1,8 @@
-let respostas = [
-    prompt("Adivinhe jogador1").trim(),
-    prompt("Adivinhe jogador2").trim(),
-    prompt("Adivinhe jogador3").trim(),
-    prompt("Adivinhe jogador4").trim(),
-    prompt("Adivinhe jogador5").trim()
-];
+let username = prompt("Adivinhe o nome");
+let password = prompt("Adivinhe o enigma");
 
-let corretos = [
-    "Emmanuella Pinheiro",
-    "Evandro Santos",
-    "Emily Marques",
-    "Eliseu Pinto",
-    "Evelyn Rocha"
-];
-
-let acertou = respostas.length === corretos.length &&
-               respostas.every(nome => corretos.includes(nome));
-
-if (acertou) {
-    document.write("Parabéns continue sua jornada");
-} else {
-    document.write("Tente de novo seu bastardo");
-}
+if (username === "admin" && password === "password") {
+    document.write("Parabéns agora continue sua jornada");
+        } else {
+    document.write("Tente denovo seu bastardo");
+    }
